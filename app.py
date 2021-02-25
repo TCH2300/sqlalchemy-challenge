3b0e6edb62bfcf71714cea82ab0f9b30c674e618
Tricia Harris <triciaharris@Tricias-MBP.attlocal.net>
2021-02-24 19:17:19 -0800

    update

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
+# Import Dependencies
+%matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# view all of the classes that automap found
+# Base.classes.keys() 
+inspector=inspect(engine)
+inspector.get_table_names()
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+@app.route("/")
+def Home page():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+    )
 
